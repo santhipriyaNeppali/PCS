216a86c8fcc405b922626ad31d847365da18ed73
santhipriyaNeppali <santhipriya.558@gmail.com>
2020-03-23 02:26:34 +0530

    JS documentation added

--- pcs-server/api/api.js
+++ pcs-server/api/api.js
@@ -4,12 +4,22 @@ const api_url = process.env.api_url || "http://v0.postcodeapi.com.au/";
 
 const api_suburbs_url = api_url+"suburbs.json";
 
+/**
+ * Returns Promise of Http request to get suburb based on postcode
+ * from rest API
+ * @param params request params of client URL
+ */
 async function getSuburbs(params) {
     const response = await fetch(api_suburbs_url+"?postcode="+params.code+"");
     const json =  await response.json();
     return json;
 }
 
+/**
+ * Returns Promise of Http request to get nearby suburbs based on lat,lon and distance
+ * from rest API
+ * @param params request params of client URL
+ */
 async function getSuburbsByDistance(params) {
     const response = await fetch(api_url+"radius.json?latitude="+params.latitude+
     "&longitude="+params.longitude+"&distance="+params.distance);
--- pcs-server/routes/route.js
+++ pcs-server/routes/route.js
@@ -2,6 +2,9 @@
     const express = require('express');
     const router = express.Router();
 
+    /**
+     * Handles the routing for suburb search with postcode
+     */
     router.get('/suburbs/:code' , (req,res,next)=>{
         const response = api.getSuburbs(req.params);
         var result = response.then(
@@ -15,7 +18,10 @@
             }
         )});
     });
-    
+
+    /**
+     * Handles the routing for narby suburbs search with lat, lon and distance (radius)
+     */
     router.get('/suburbs/:latitude/:longitude/:distance' , (req,res,next)=>{
         const response = api.getSuburbsByDistance(req.params);
         var result = response.then(
